8475f66a39504e6c1c742bf68b53cc5057e171a7
Igor <ruvinovigor@gmail.com>
2019-09-11 00:56:35 -0400

    Udemy-Webdev: JavaScript 3 (Drums)
    Added event listener for keyboard inputs and mapped to corresponding drum buttons and sounds. Added animations when drum buttons are pressed.

--- !Courses/Udemy - Complete Web Dev Bootcamp/JS-Drumkit/index.js
+++ !Courses/Udemy - Complete Web Dev Bootcamp/JS-Drumkit/index.js
@@ -3,38 +3,59 @@ var buttonsList = document.querySelectorAll(".drum");
 for (var i = 0; i < buttonsList.length; i++) {
   // This is called an anonymous function (has no name)
   buttonsList[i].addEventListener("click", function() {
-    // 'this' contains the identity of the button which triggered the event
-    switch (this.innerHTML) {
-      case "w":
-        var audio = new Audio("sounds/tom-4.mp3");
-        audio.play();
-        break;
-      case "a":
-        var audio = new Audio("sounds/tom-3.mp3");
-        audio.play();
-        break;
-      case "s":
-        var audio = new Audio("sounds/tom-2.mp3");
-        audio.play();
-        break;
-      case "d":
-        var audio = new Audio("sounds/tom-1.mp3");
-        audio.play();
-        break;
-      case "j":
-        var audio = new Audio("sounds/snare.mp3");
-        audio.play();
-        break;
-      case "k":
-        var audio = new Audio("sounds/kick-bass.mp3");
-        audio.play();
-        break;
-      case "l":
-        var audio = new Audio("sounds/crash.mp3");
-        audio.play();
-        break;
-      default:
-        break;
-    }
+    playSoundFromKey(this.innerHTML);
+    buttonAnimation(this.innerHTML);
   });
+}
+
+// Passing in event allows us to track event that triggered the listener
+// event contains lots of information including key pressed, etc.
+document.addEventListener("keydown", function(event) {
+  playSoundFromKey(event.key);
+  buttonAnimation(event.key);
+});
+
+function playSoundFromKey(key) {
+  var audio;
+
+  switch (key) {
+    case "w":
+      audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case "a":
+      audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "s":
+      audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "d":
+      audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "j":
+      audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    case "k":
+      audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    case "l":
+      audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    default:
+      break;
+  }
+}
+
+function buttonAnimation(key) {
+  document.querySelector("." + key).classList.add("pressed");
+  setTimeout(function() {
+    document.querySelector("." + key).classList.remove("pressed");
+  }, 50);
+
 }
