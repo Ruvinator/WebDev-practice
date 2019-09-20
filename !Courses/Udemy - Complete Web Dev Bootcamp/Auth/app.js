3fcc97e135d12779232563650d7b0ff35fa01fc1
Igor <ruvinovigor@gmail.com>
2019-09-20 00:39:46 -0400

    Udemy-Webdev: Auth Level2
    - Add encryption to new users' passwords using a secret string stored in server JavaScript file

--- !Courses/Udemy - Complete Web Dev Bootcamp/Auth/app.js
+++ !Courses/Udemy - Complete Web Dev Bootcamp/Auth/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -19,6 +20,14 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
+});
+
+// Add plugin to mongoose schema before defining model
+// Mongoose will automatically be encrypting and decrypting on save and find
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, {
+  secret: secret,
+  encryptedFields: ["password"]  // want to search email so don't encrypt it
 });
 
 const User = new mongoose.model("User", userSchema);
@@ -41,7 +50,7 @@ app.get("/register", function(req, res) {
   res.render("register");
 });
 
-app.post("/register", function (req, res) {
+app.post("/register", function(req, res) {
   const newUser = new User({
     email: req.body.username,
     password: req.body.password
@@ -56,11 +65,13 @@ app.post("/register", function (req, res) {
   });
 });
 
-app.post("/login", function (req, res) {
+app.post("/login", function(req, res) {
   const username = req.body.username;
   const password = req.body.password;
 
-  User.findOne({email: username}, function(err, foundUser) {
+  User.findOne({
+    email: username
+  }, function(err, foundUser) {
     if (err) {
       console.log(err);
     } else {
