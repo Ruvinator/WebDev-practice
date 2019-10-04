166b2fd6416abab7dfa819448a9d78f0a4ec9940
Igor <ruvinovigor@gmail.com>
2019-10-04 00:56:49 -0400

    Udemy-React: Burger Builder 7 (Header)
    - Implement header with corresponding styles and placeholder items
    - Add logo component to header and style accordingly

--- 0_Courses/Udemy-React/burger-builder/src/components/Layout/Layout.js
+++ 0_Courses/Udemy-React/burger-builder/src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AuxComp from '../../hoc/AuxComp';
+import Toolbar from '../Navigation/Toolbar/Toobar';
 
 import styles from './Layout.module.css';
 
 const layout = props => (
     <AuxComp>
+        <Toolbar />
         <div>Toolbar, SideDrawer, Backdrop</div>
         <main className={styles.Content}>
             {props.children}
