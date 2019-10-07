e21fd613068416019c2838e0770f87b2ae5a8195
Igor <ruvinovigor@gmail.com>
2019-10-06 20:59:30 -0400

    Udemy-React: Redux 5 (Middleware)
    - Implement middleware for debugging redux code (ex. logging actions right before they enter reducer)
    - Add code required for interaction with Chrome extension Redux Devtools

--- 0_Courses/Udemy-React/redux-start/src/index.js
+++ 0_Courses/Udemy-React/redux-start/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 
 import './index.css';
@@ -15,9 +15,33 @@ const rootReducer = combineReducers({
     resultReducer:  resultReducer
 });
 
+// Middleware can be added at point in time where store is initialized
+// Function below is simple middleware which logs each issued action
+// All functions below are executed automatically by redux
+const logger = store => {
+    // Returns function which can be executed to let action continue to reducer
+    return next => {
+        // Returns another function which receives action dispatched as input
+        return action => {
+            // Code here is run between action and reducer
+            console.log('[Middleware] Dispatching', action);
+
+            // Allows action to continue 
+            const result = next(action);
+
+            console.log('[Middleware] Next state', store.getState())
+            return result;
+        }
+    }
+};
+
+// Used for React Devtools extension (holds a function)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 // Store created right when application launches; this is a good place
 // Reducer is created in separate file and included here
-const store = createStore(rootReducer);
+// compose combines multiple enhancers (middleware)
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
 
 // Store is connected to React app using Provider and input store argument
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
